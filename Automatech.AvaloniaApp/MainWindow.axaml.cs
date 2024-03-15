fb22415bf35ab543e5a8dd83b3a676c34e6af32b
章征宇 <@ZHANGZHENGYU>
2024-03-15 10:42:48 +0800

    新增Avalonia项目

---
+++ Automatech.AvaloniaApp/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Automatech.AvaloniaApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
